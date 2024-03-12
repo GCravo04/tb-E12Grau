fdb686c1a105dd05399e7eb730797539b8b46361
gabic <gabic@MSI>
2024-03-12 09:32:50 +0000

    Eq2Grau algoritmo respondido

--- Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 using Eq2Grau.Models;
+
 using Microsoft.AspNetCore.Mvc;
+
 using System.Diagnostics;
 
 namespace Eq2Grau.Controllers
 {
+
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,37 +19,134 @@ namespace Eq2Grau.Controllers
 
 
         /// <summary>
-        /// Portal de entrada dos pedidos feitos pelo browser
-        /// Método para calculas as raízes de uma equação de 2º grau
+        /// Porta de entrada do pedidos feitos pelo Browser.
+        /// Método para calcular as raízes de uma equação do 2º grau
         /// </summary>
-        /// <param name="A">Parametro para x^2</param>
-        /// <param name="B">Parametro para x</param>
-        /// <param name="C">Parametro para num independente</param>
-        /// <returns></returns>       
-        public IActionResult Index(string A,  string B, string C)
+        /// <param name="A">parâmetro do x^2</param>
+        /// <param name="B">parâmetro do x</param>
+        /// <param name="C">parâmetro independente</param>
+        /// <returns></returns>
+        public IActionResult Index(string A, string B, string C)
         {
-            /*Algoritmo
-             * 1- Ler os parametros A,B e C
-             * 2- Validar se pode fazer as Operações
-             *  2.1- Têm de ser números
-             *  2.2- A tem de ser diferente de 0
-             * 3-Se posso calcular,:
-             *  3.1- determinar as raízes
-             *      3.1.1 x=((-b +/- sqr(b^2-4ac))/(2a))
-             *      3.1.2 cacular o delta : b^2-4ac
-             *          3.1.2.1 se o delta > 0 , há 2 raizes distintas , x1 e x2
-             *          3.1.2.2 se delta = 0 , há duas raizes iguais , x1=x2
-             *          3.1.2.3 se delta < 0 , há 2 raizes complexas conjugadas , x1 = -b/(2a) + sqrt(-delta)/(2a) i
-             *                                                                    x2 = -b/(2a) - sqrt(-delta)/(2a) i
-             *  3.2 - Enviar a resposta para o cliente
+            /* ALGORITMO:
+             * 1- ler parâmetros A, B, C
+             * 2- validar se se pode fazer operações com eles
+             *    2.1- têm de ser números
+             *    2.2- A != 0
+             * 3- se posso calcular,
+             *    3.1- determinar as raízes
+             *         x=(-b +/- sqrt(b^2-4ac))/2/a
+             *         3.1.1- calcular o delta: b^2-4ac
+             *         3.1.2- se delta > 0, há 2 raízes reais distintas
+             *                              x1 e x2
+             *                se delta = 0, há 2 raízes reais iguais
+             *                              x1=x2
+             *                se delta < 0, há duas raízes complexas conjugadas
+             *                              x1 = -b/(2a) + sqrt(-delta)/(2a) i
+             *                              x2 = -b/(2a) - sqrt(-delta)/(2a) i
+             *   3.2- enviar a resposta para o cliente
              * se não posso calcular (else)
-             *   enviar mensagem de erro para o cliente (browser)
+             *    enviar mensagem de erro para o cliente (browser)
              */
 
+            // vars. auxiliares
+            double auxA = 0, auxB = 0, auxC = 0;
 
-            double a = double.Parse(A);
-            double b = double.Parse(B);
-            double c = double.Parse(C);
+
+            // 1.
+            if (string.IsNullOrWhiteSpace(A) || string.IsNullOrWhiteSpace(B) || string.IsNullOrWhiteSpace(C))
+            {
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+            // 1.
+            if (!double.TryParse(A, out auxA))
+            {
+                // o A não é número.
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+            // 1.
+            if (!double.TryParse(B, out auxB))
+            {
+                // o B não é número.
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+            // 1.
+            if (!double.TryParse(C, out auxC))
+            {
+                // o C não é número.
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+
+            // 2.
+            if (auxA == 0)
+            {
+                // o A é ZERO.
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+
+            // 3.
+            double delta = Math.Pow(auxB, 2) - 4 * auxC;
+            // 3.1
+            if (delta > 0)
+            {
+                string x1 = Math.Round((-auxB + Math.Sqrt(delta)) / 2 / auxA, 2) + "";
+                string x2 = Math.Round((-auxB - Math.Sqrt(delta)) / 2 / auxA, 2) + "";
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+            if (delta == 0)
+            {
+                string x = Math.Round(-auxB / 2 / auxA, 2) + "";
+
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+            if (delta < 0)
+            {
+                string x1 = Math.Round((-auxB) / 2 / auxA, 2) + " + " + Math.Round(Math.Sqrt(-delta) / 2 / auxA, 2) + " i";
+                string x2 = Math.Round((-auxB) / 2 / auxA, 2) + " - " + Math.Round(Math.Sqrt(-delta) / 2 / auxA, 2) + " i";
+                // enviar mensagem para o utilizador
+
+
+                // devolver controlo à View
+                return View();
+            }
+
+            // se chegar aqui, alguma coisa correu muito mal...
+            // devolver controlo à View
             return View();
         }
 
